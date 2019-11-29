99f328431cc49b1c201585cb8eaf0e5c93057e30
Hiram Fernandes <hiram.fernandes@poatek.com>
2019-11-29 13:28:44 -0300

    Working version with DB

---
+++ src/ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebAPI.Data;
+using ProAgil.WebAPI.Model;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly Evento[] listaEventos;
+        private DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+            listaEventos = new Evento[]
+            {
+                new Evento(){
+                    EventoId = 1,
+                    Tema = "Angular e .Net Core",
+                    Local = "Belo Horizonte",
+                    Lote = "1o Lote"
+                    },
+                new Evento()
+                {
+                    EventoId = 2,
+                    Tema = "Angular",
+                    Local = "São Paulo",
+                    Lote = "1o Lote"
+                },
+            };
+        }
+
+        [HttpGet("{id}")]
+        public async  Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var eventos = _context.Eventos;
+                return Ok(await eventos.FirstOrDefaultAsync(x => x.EventoId == id));
+            }
+            catch (SystemException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            try
+            {
+                var eventos = await _context.Eventos.ToListAsync();
+                return Ok(eventos);
+            }
+            catch (SystemException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
